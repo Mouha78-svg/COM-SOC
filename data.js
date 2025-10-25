106e18f171169f87786fbb724b503405fe25f9db
Mouha78-svg <mseye792@gmail.com>
2025-10-25 16:46:46 +0000

    update site com-soc

---
+++ data.js
@@ -0,0 +1,127 @@
+const people = [
+  {
+    img: "https://www.course-api.com/images/people/person-3.jpeg",
+    name: "Fodé Moussa Diatta",
+    job: "Secrétaire Générale",
+    text: `+221 78 200 95 10`,
+  },
+  {
+    img: "https://www.course-api.com/images/people/person-3.jpeg",
+    name: "cheikh ibra diéye",
+    job: "Secrétaire exécutif",
+    text: `+221 78 140 26 44`,
+  },
+  {
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "ousseyenou niang",
+    job: "Secrétaire administratif",
+    text: `+221 78 766 04 63`,
+  },
+  {
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "Papa Amadou DIAKHATE",
+    job: "Trésorier Général",
+    text: `+221 78 282 39 26`,
+  },
+  {
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "Abdourahrnane Pouye SONKO",
+    job: "Sous-commission AnimationCulturelle et Sportive (ACS)",
+    text: `+221 78 299 1021`,
+  },
+  {
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "Khalifa Ababacar BASSENE",
+    job: "Sous-commission Animation culturelle et Sportive (ACS)",
+    text: `+221 78 023 41 48`,
+  },
+  {
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "El Hadji Birane SENE",
+    job: "Sous—commission Bourses Cartes Transport (BCT)",
+    text: `+221 78 968 32 77`,
+  },
+  {
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "Ousseynou ba",
+    job: "Sous—commission Bourses Cartes Transport (BCT)",
+    text: `+221 78 541 84 09`,
+  },
+  {
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "Ismaila diagne",
+    job: "Sous-commission Hébergement",
+    text: `+221 78 200 65 18`,
+  },
+  {
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "Hamza SOUMBOUNDOU",
+    job: "Sous—commission Hébergement",
+    text: `+221 78 698 12 36`,
+  },
+  {
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "Gabrielle Marie Noëlle diouf",
+    job: "Sous—commission Médicosocial",
+    text: `+221 78 156 99 24`,
+  },
+  {
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "Moustapha kane",
+    job: "Sous-commission Médicosocial",
+    text: `+221 77 249 55 85`,
+  },
+  {
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "Guinda doucoure",
+    job: "Sous-commission Restauration",
+    text: `+221 77 793 8946`,
+  },
+  {
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "Abdoul Aziz souane",
+    job: "Sous-commission Restauration",
+    text: `+221 77 342 20 10`,
+  },
+  {
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "Moustapha dione",
+    job: "Comité Emploi, Entreprenariat et Stage",
+    text: `+221 77 898 79 03`,
+  },
+  ,
+  {
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "A bdoul Khadre A. DIEYLANI",
+    job: "Comité Emploi, Entreprenariat et Stage",
+    text: `+221 77 869 33 83`,
+  },
+  {
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "Mouhamed Gaye sow",
+    job: "Commissaires aux Comptes",
+    text: `+221 77 205 87 45`,
+  },
+  {
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "Moussa boussou",
+    job: "Commissaires aux Comptes",
+    text: `+221 70 209 03 73`,
+  },
+  {
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "Sékou BIAYE",
+    job: "Assesseurs",
+    text: `+221 76 412 43 03`,
+  },
+  {
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "Oumar gassama",
+    job: "Assesseurs",
+    text: `+221 78 683 06 03`,
+  },
+];
+
+console.log(people.length);
+
+export default people;
--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
+import people from "../data.js";
 // ********** set date ************
 // select span
 // const date = (document.querySelector(".date").innerHTML =
 //   new Date().getFullYear());
+const container = document.querySelector(".slide-container");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
 const date = document.querySelector("#date");
 date.textContent = new Date().getFullYear();
 // ********** nav toggle ************
@@ -43,4 +47,66 @@ scrollLinks.forEach((link) => {
       behavior: "smooth",
     });
   });
+});
+container.innerHTML = people
+  .map((person, slideindex) => {
+    const { img, name, job, text } = person;
+    let position = "next";
+    if (slideindex === 0) {
+      position = "active";
+    }
+    if (slideindex === people.length - 1) {
+      position = "last";
+    }
+    return `
+    <article class="slide ${position}">
+           <img src="${img}" class="slide-img" alt="${name}">
+           <h4 >${name}</h4>
+           <p class="title-slide">${job}</p>
+           <p class="text-slide">
+           <span><i class="fa fa-phone"></i></span>
+            ${text}
+           </p>
+           <div class="quote-icon">
+            <div class="fas fa-quote-right"></div>
+           </div>
+          </article>
+  `;
+  })
+  .join(" ");
+
+const startSlider = (type) => {
+  const active = document.querySelector(".active");
+  const last = document.querySelector(".last");
+  let next = active.nextElementSibling;
+  if (!next) {
+    next = container.firstElementChild;
+  }
+  active.classList.remove(["active"]);
+  last.classList.remove(["last"]);
+  next.classList.remove(["next"]);
+
+  if (type === "prev") {
+    active.classList.add("next");
+    last.classList.add("active");
+    next = last.previousElementSibling;
+
+    if (!next) {
+      next = container.lastElementChild;
+    }
+    next.classList.remove(["next"]);
+    next.classList.add("last");
+    return;
+  }
+
+  active.classList.add("last");
+  last.classList.add("next");
+  next.classList.add("active");
+};
+
+prevBtn.addEventListener("click", () => {
+  startSlider("prev");
+});
+nextBtn.addEventListener("click", () => {
+  startSlider();
 });
